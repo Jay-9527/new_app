8da738cb5e8fa1702f80293751dad5db32cc0cd4
adiao <1819192616@qq.com>
2023-04-17 12:26:43 +0800

    update table develptment

--- src/main.js
+++ src/main.js
@@ -7,9 +7,13 @@ import "@popperjs/core"
 import "bootstrap"
 import './scss/global.scss'
 import store from './utils/vuex/store'
+import 'xe-utils'
+import VXETable from 'vxe-table'
+import 'vxe-table/lib/style.css'
 
 const app = createApp(App)
 
+app.use(VXETable)
 app.use(store)
 app.use(router)
 app.mount('#app')
